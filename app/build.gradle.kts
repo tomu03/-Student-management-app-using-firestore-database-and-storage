42295fd855c13d76bd6b7ef46beff6ab0e8ddd6c
tomu03 <369tomu@gmail.com>
2024-05-25 11:43:47 +0600

    Student list added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+//added to
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
